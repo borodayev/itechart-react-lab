312a9d4bd6f4af8b5a427b10ac35c2862189c6b1
Valeriy Borodayev <borodayev@Valeriys-MacBook-Air.local>
2021-09-14 18:45:23 +0600

    feat: add an endpoint with products, set up nodemon

--- src/server.ts
+++ src/server.ts
@@ -6,9 +6,29 @@ const app: Application = express();
 
 app.use(express.static('dist'));
 
-app.get('*', (_, res) => {
+app.get('/', (_, res) => {
   const rootHtmlPath = path.join(__dirname, '/dist/index.html');
   res.sendFile(rootHtmlPath);
+});
+
+app.get('/products', (_, res) => {
+  const products = [
+    {
+      displayName: 'Cyberpank 2077',
+      price: '60$'
+    },
+    {
+      displayName:
+        'SpongeBob SquarePants: Battle for Bikini Bottom – Rehydrated',
+      price: '40$'
+    },
+    {
+      displayName: 'God Of War',
+      price: '50$'
+    }
+  ];
+
+  res.send(products);
 });
 
 app.listen(PORT, () => console.log(`App is running on ${PORT}.`));
